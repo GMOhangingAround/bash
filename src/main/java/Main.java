2dfb488a5232ed2ff94076df6d67fd868cc6b639
GMOhangingAround <gail.grikienyte@gmail.com>
2026-02-04 16:10:06 +0000

    Add scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+
+        Scanner scanner = new Scanner(System.in); // Start scanner
+
+        String command = scanner.nextLine(); // Read next line 
+
+        System.out.println(command);
+
+        scanner.close();
     }
 }
